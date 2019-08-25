5292cf33a98393d9a52f697eb3dec85ea1c293ef
Janay Bailey <tjanay87@gmail.com>
2019-08-25 21:13:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
-  counter = 0
+  counter = ("1","4","9")
 
     while array[counter] do
       puts array[counter]
-      counter += 1
+      counter += 1.sqrt
     end
 end
